c1adbed6d5c3e0c6537f87a07b441f464da3052a
Vibjerg <vibjerg@gmail.com>
2017-02-28 12:44:03 +0100

    refactor client

--- src/client/state/client.js
+++ src/client/state/client.js
@@ -1,21 +1,38 @@
+/**
+ * @file
+ * Client for calling the API.
+ */
+
 import request from 'superagent';
 
+/**
+ * Get hierarchy for index.
+ *
+ * @param index
+ * @returns {Promise}
+ */
 export function hierarchy(index) {
+  return call('hierarchy', {q: index});
+}
+
+/**
+ * Call endpoint on API.
+ *
+ * @param url
+ * @param query
+ * @returns {Promise}
+ */
+function call(url, query) {
   return new Promise((resolve, reject) => {
-    request('/api/hierarchy')
-      .query({q: index})
+    request(`/api/${url}`)
+      .query(query)
       .end((err, res) => {
         if (err) {
           reject(err);
         }
         else {
           const result = JSON.parse(res.text).result;
-          if (Object.keys(result).length) {
-            resolve(result);
-          }
-          else {
-            reject({});
-          }
+          resolve(result);
         }
       });
   });
