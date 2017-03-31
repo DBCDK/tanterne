00acbccbfdecc21f473c83e422696d547e71a995
Søren Vibjerg <vibjerg@gmail.com>
2017-03-31 15:04:52 +0200

    More tests

--- integration/tests/hierarchy.test.js
+++ integration/tests/hierarchy.test.js
@@ -5,35 +5,45 @@ chai.should();
 
 describe('Testing Hierarchy', () => {
   it('Should render hierarchy', () => {
-    browser.url('/#!/hierarchy/65.2');
+    browser.url('/#!/hierarchy/40-49');
     browser.pause(200);
 
-    const text = browser.getText('.selected .dk5');
-    assert.equal(text, '65.2', 'title is present');
+    const text = browser.getText('.selected .dk5')[0];
+    assert.equal(text, '40-49', 'title is present');
 
-    const topics = browser.getText('.selected .hierarchy-topics');
-    assert.contains(topics, 'Administrativ IT', 'first topic is present');
-
-    const levels = browser.getText('.selected .hierarchy-level');
-    assert.contains(levels, 'Bogføring. Bogholderi', 'sublevel is present');
+    const level = browser.getText('.selected .hierarchy-level')[0];
+    assert.include(level, 'Andre verdensdele', 'sublevel is present');
   });
 
   it('Click on sublevel', () => {
-    browser.url('/#!/hierarchy/65.2');
+    browser.url('/#!/hierarchy/40-49');
     browser.pause(200);
-    browser.click('.selected .hierarchy-level a');
-    browser.pause(200);
+    browser.click('[href="#!/hierarchy/40"]');
+    browser.pause(20000);
 
-    const text = browser.getText('.selected .dk5');
-    assert.equal(text, '65.21', 'title is present');
+    const text = browser.getText('.selected h2 .dk5')[0];
+    assert.equal(text, '40', 'title is present');
 
-    const topics = browser.getText('.selected .hierarchy-topics');
-    assert.contains(topics, 'Bogføring  - Enkelte fags bogføring må søges under emnet', 'first topic + note is present');
+    const topics = browser.getText('.selected .hierarchy-topics')[0];
+    assert.include(topics, 'Flyveulykker', 'first topic is present');
   });
 
-  it('Click on toplevel', () => {
-    browser.url('/#!/hierarchy/65.2');
+  it('Click between levels', () => {
+    browser.url('/#!/hierarchy/40-49');
     browser.pause(200);
-    browser.click('.level-1 a');
+    assert.equal(browser.getText('.selected .dk5')[0], '40-49', 'toplevel is selected');
+
+    browser.click('[href="#!/hierarchy/40"]');
+    browser.pause(200);
+    assert.equal(browser.getText('.selected .dk5')[0], '40', '2. level is selected');
+
+    browser.click('[href="#!/hierarchy/40.6"]');
+    browser.pause(200);
+    assert.equal(browser.getText('.selected .dk5')[0], '40.6', '3. level is selected');
+
+    browser.click('[href="#!/hierarchy/40-46"]');
+    browser.pause(200);
+    assert.equal(browser.getText('.selected .dk5')[0], '40-46', 'toplevel is selected');
+
   });
 });
