6098384901bc295e6400685eb44dfe7e50769799
Martin Møller <mmj@dbc.dk>
2017-04-18 14:14:18 +0200

    Remove items from within the cart

--- src/client/components/Comparer/ComparerContainer.component.js
+++ src/client/components/Comparer/ComparerContainer.component.js
@@ -1,4 +1,27 @@
 import React from 'react';
+import {CartButton} from '../Cart/CartButton.component';
+
+/**
+ *
+ * @param {Object} data
+ * @param {React.ComponentElement} cartButton
+ * @return {React.ComponentElement | null}
+ * @constructor
+ */
+function CamparerItem({data, cartButton}) {
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <div className="comparer--item">
+      <span>{data.index}</span>
+      <span>{data.title}</span>
+      <span>{data.note}</span>
+      <span>{cartButton}</span>
+    </div>
+  );
+}
 
 export default class ComparerContainer extends React.Component {
   constructor(props) {
@@ -6,9 +29,18 @@ export default class ComparerContainer extends React.Component {
   }
 
   render() {
+    const items = Object.keys(this.props.cart.contents).map((index) => {
+      const data = this.props.cart.contents[index].data;
+      const cartButton = <CartButton index={index} cart={this.props.cart}/>;
+      return <CamparerItem data={data} cartButton={cartButton} />;
+    });
+
     return (
-      <div id="comparer" className={Object.keys(this.props.cart.contents).length ? 'show-cart' : ''}>
-        <h2>Hest</h2>
+      <div id="comparer" className={items.length ? 'show-cart' : 'show-cart'}>
+        <div className="comparer--content">
+          <span className="comparer--content--headline">Sammenlign poster</span>
+          <div className="comparer--content--items-container">{items}</div>
+        </div>
       </div>
     );
   }
