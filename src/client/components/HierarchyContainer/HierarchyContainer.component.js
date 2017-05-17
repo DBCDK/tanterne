c70bfdd081e598f6a05de9da88d7c0359b86bf86
Finn Stausgaard <fvs@dbc.dk>
2017-05-17 14:31:31 +0200

    #170: change color for decommissioned indexes

--- src/client/components/HierarchyContainer/HierarchyContainer.component.js
+++ src/client/components/HierarchyContainer/HierarchyContainer.component.js
@@ -133,7 +133,8 @@ function HierarchyElement({topics, description = '', pro = false, noteSystematic
  * @constructor
  */
 function HierarchyLevel({hierarchy, Header = 'h2', level = 1, selected, pro, cart}) {
-  const {index, title, hasChildren, children, items, note, noteSystematic, noteSystematicHistoric} = hierarchy;
+  console.log('hier', hierarchy);
+  const {index, title, decommissioned, hasChildren, children, items, note, noteSystematic, noteSystematicHistoric} = hierarchy;
   const isSelected = selected === index;
 
   let contains = children;
@@ -143,6 +144,7 @@ function HierarchyLevel({hierarchy, Header = 'h2', level = 1, selected, pro, car
 
   const cartButton = level >= 2 && pro ? <CartButton {...{index, cart}} /> : null;
   const infoChildren = pro & hasChildren ? 'hasChildren' : '';
+  const infoDecommissioned = pro & decommissioned ? 'decommissioned' : '';
 
   return (
     <div className={`hierarchy-level level level-${level}`}>
@@ -150,7 +152,7 @@ function HierarchyLevel({hierarchy, Header = 'h2', level = 1, selected, pro, car
         <Header className={`${isSelected && 'hierarchy-level--header' || ''}`}>
           {cartButton}
           <Link to={`/hierarchy/${index}`}>
-            <span className={`dk5 ${infoChildren}`}>{index}</span>
+            <span className={`dk5 ${infoDecommissioned} ${infoChildren}`}>{index}</span>
             <span className="name">{title}</span>
             {isSelected && !contains && <div className="hierarchy-spinner">{<Spinner size="small-light"/>}</div>}
           </Link>
