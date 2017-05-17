62c68deb995d39cf1b985ebe8b3db4898a70534f
Martin Møller <mmj@dbc.dk>
2017-05-17 10:28:18 +0200

    added test

--- integration/tests/hierarchy.mobile.test.js
+++ integration/tests/hierarchy.mobile.test.js
@@ -22,4 +22,26 @@ describe('Testing Hierarchy in mobile view', () => {
     browser.waitForVisible('.search-field--button--text', 1000);
     assert.equal(browser.getUrl(), 'http://localhost:4013/');
   });
+
+  it('Should navigate back to the frontpage', () => {
+    browser.url('/#!/hierarchy/00.109');
+    browser.waitForVisible('.hierarchy--navbar--title', 1000);
+    assert.isTrue(browser.getAttribute('.hierarchy--navbar--href', 'href').includes('/#!/hierarchy/00.1'));
+
+    browser.click('.hierarchy--navbar--href');
+    browser.pause(200);
+    assert.isTrue(browser.getAttribute('.hierarchy--navbar--href', 'href').includes('/#!/hierarchy/00'));
+
+    browser.click('.hierarchy--navbar--href');
+    browser.pause(200);
+    assert.isTrue(browser.getAttribute('.hierarchy--navbar--href', 'href').includes('/#!/hierarchy/00-07'));
+
+    browser.click('.hierarchy--navbar--href');
+    browser.pause(200);
+    assert.isTrue(browser.getAttribute('.hierarchy--navbar--href', 'href').includes('/'));
+
+    browser.click('.hierarchy--navbar--href');
+    browser.pause(200);
+    assert.isFalse(browser.isVisible('.hierarchy--navbar'));
+  });
 });
