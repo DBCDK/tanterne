d0c55c058247eb14eb83dfee3d10c5710fc7efc2
Finn Stausgaard <fvs@dbc.dk>
2017-06-08 13:16:41 +0200

    #223: lint

--- src/client/components/HelpContainer/HelpContainer.component.js
+++ src/client/components/HelpContainer/HelpContainer.component.js
@@ -8,123 +8,125 @@ import React, {Component} from 'react';
 export class HelpContainerComponent extends Component {
   proHelp() {
     if (this.props.pro) {
-    return <div className='help-pro--container'>
-             <a name="supplNumbers"/>
-             <div className="section-head">
-               DK5 - Till&aelig;gstal
-             </div>
-             <div className="section">
-               Dansk bogfortegnelse anvender ved underdeling af sk&oslash;nlitteraturen et system af till&aelig;gstal,
-               som adskilles fra det egentlige klassem&aelig;rke ved en bindestreg.
-               Disse tal bruges i stedet for decimalerne 1 og 2,
-               hvis anvendelse i decimalklassedelingen er beskrevet i noten til 82-88.
-               Till&aelig;gstallene vil ogs&aring; kunne finde anvendelse ved anden bibliografisk systematik over sk&oslash;nlitteratur.
-             </div>
-             <div className="section">
-               <i>Till&aelig;gstallene for sk&oslash;nlitteratur</i> best&aring;r af mindst to cifre,
-               hvoraf det f&oslash;rste angiver publiceringssproget,
-               og det sidste dels angiver digtarten,
-               dels markerer antologiform.
-               For b&oslash;rne- og ungdomslitteraturens vedkomne indskydes mellem 1. og 2. ciffer et 9-tal, der markerer m&aring;lgruppen.
-             </div>
-             <div className="section">
-               1. <i>ciffer</i> tildeles efter nedenst&aring;ende tabel:
-               <div><div className="bullet">0</div>Skrifter p&aring; originalsproget</div>
-               <div><div className="bullet">1</div>Skrifter oversat til fremmesprog</div>
-               <div><div className="bullet">2</div>Skrifter oversat til dansk</div>
-             </div>
-             <div className="section">
-               2. <i>ciffer</i> tildeles efter nedenst&aring;ende tabel:
-               <div><div className="bullet">0</div>Digte. Antologier</div>
-               <div><div className="bullet">1</div>Digte. V&aelig;rker af enkelte forfattere
-                 <div className="note">
-                   Till&aelig;gstalene 0 og 1 omfatter ikke epsisk og dramatisk litteratur p&aring; vers.
-                   Kortere fort&aelig;llende digte placeres dog i formgrupperne 0 og 1.
-                 </div>
-               </div>
-               <div><div className="bullet">2</div>Skuespil. Antologier</div>
-               <div><div className="bullet">3</div>Skuespil. V&aelig;rker af enkelte forfattere</div>
-               <div><div className="bullet">4</div>Romaner og noveller. Antologier
-                 <div className="note">
-                   Her anbringes ogs&aring; antologier, sammensat af forskellige digtarter<br />
-                   I grupperne 86 og 88 placerer Dansk bogfortegnelse l&aelig;seb&oslash;ger og prosaantologier til skolebrug i en undergruppe med till&aelig;gstallet -046 eller -246.
-                   Andre skoleudgaver af sk&oslash;nlitteratur placeres som tilsvarende almindelige udgaver.
-                 </div>
-              </div>
-              <div><div className="bullet">5</div>Romaner og noveller f&oslash;r 1901. V&aelig;rker af enkelte forfattere</div>
-              <div><div className="bullet">6</div>Romaner og noveller fra 1901. V&aelig;rker af enkelte forfattere</div>
-              <div><div className="bullet">7</div>Humor, tegneserier etc. Antologier
-                <div className="note">
-                  Her anbringes samlinger af vittigheder og humoristiske sm&aring;stykker.
-                </div>
-              </div>
-              <div><div className="bullet">8</div>Humor, tegneserier etc. af enkelte forfattere</div>
-              <div><div className="bullet">9</div>B&oslash;rne- og ungdomsb&oslash;ger</div>
-              <div><div className="bullet">90</div>Digte for b&oslash;rn. Antologier</div>
-              <div><div className="bullet">91</div>Digte for b&oslash;rn. Enkelte forfattere</div>
-              <div><div className="bullet">92</div>B&oslash;rnekomedier. Antologier</div>
-              <div><div className="bullet">93</div>B&oslash;rnekomedier. Enkelte forfattere</div>
-              <div><div className="bullet">94</div>Fort&aelig;llinger for b&oslash;rn. Antologier
-                <div className="note">
-                  Her anbringes ogs&aring; antologier sammensat af forskellige digtarter.
-                </div>
-              </div>
-              <div><div className="bullet">96</div>Fort&aelig;llinger for b&oslash;rn. Enkelte forfattere</div>
-              <div><div className="bullet">97</div>Humor, tegneserier etc. for b&oslash;rn. Antologier</div>
-              <div><div className="bullet">98</div>Humor, tegneserier etc. for b&oslash;rn. Enkelte forfattere</div>
-             </div>
-             <div className="section">
-               Systemet med till&aelig;gstal, anbragt efter en streg,
-               anvendes ogs&aring; i visse faglitter&aelig;re grupper for at markere en for Dansk bogfortegnelse speciel supplerende underdeling.
-             </div>
-             <div className="section">
-               <i>Faglitteratur &mdash; Till&aelig;gstal med fast betydning</i>
-               <div><div className="bullet">-6</div>Skole- og l&aelig;reb&oslash;ger (inkl. opgavesamlinger)
-                 <div className="note">
-                   Bruges i 51.3, 86-04 og 88-24.
-                 </div>
-               </div>
-               <div><div className="bullet">-61</div>Almindelige l&aelig;reb&oslash;ger, l&aelig;seb&oslash;ger
-                 <div className="note">
-                   Bruges i 89.2, 89.3 og 89.4.
-                 </div>
-               </div>
-               <div><div className="bullet">-62</div>&Oslash;velser og opgaver
-                 <div className="note">
-                   Bruges i 89.2, 89.3 og 89.4.
-                 </div>
-               </div>
-               <div><div className="bullet">-65</div>L&aelig;reb&oslash;ger (inkl. opgavesamlinger) for folkeskolen</div>
-               <div><div className="bullet">-66</div>Andre skole- og l&aelig;reb&oslash;ger (inkl. opgavesamlinger)
-                 <div className="note">
-                   -65 og -66 bruger i 40, 51, 51.1, 51.5, 51.8, 53, 54, 56 og 90,
-                 </div>
-               </div>
-               <div><div className="bullet">-8</div>Udgaver for b&oslash;rn
-                 <div className="note">
-                   Bruges i 39.12 og 88.99
-                 </div>
-               </div>
-               <div><div className="bullet">-99</div>Enkeltbiografier
-                 <div className="note">
-                   Bruges i alle grupper undtagen de alfabetisk (herunder de geografisk-topografisk) underdelte.
-                   Underdeles alfabetisk efter den biograferede persones navn.
-                 </div>
-               </div>
-             </div>
-             <div className="section">
-               <i>Faglitteratur &mdash; Till&aelig;gstal med varierende betydning</i>
-               <div><div className="bullet">46.4-0</div>Sj&aelig;lland og M&oslash;n</div>
-               <div><div className="bullet">46.4-1</div>Sams&oslash;</div>
-               <div><div className="bullet">46.4-2</div>Lolland-Falster</div>
-               <div><div className="bullet">46.4-3</div>Bronholm</div>
-               <div><div className="bullet">46.4-4</div>Fyn og de fynske &oslash;er</div>
-               <div><div className="bullet">46.4-5</div>Jylland</div>
-             </div>
-             <div className="section">
-               <div><div className="bullet">72-5</div>Humoristiske og satiriske tegninger af enkelte kunstnere.</div>
-             </div>
+    return (
+      <div className='help-pro--container'>
+        <a name="supplNumbers"/>
+        <div className="section-head">
+          DK5 - Till&aelig;gstal
+        </div>
+        <div className="section">
+          Dansk bogfortegnelse anvender ved underdeling af sk&oslash;nlitteraturen et system af till&aelig;gstal,
+          som adskilles fra det egentlige klassem&aelig;rke ved en bindestreg.
+          Disse tal bruges i stedet for decimalerne 1 og 2,
+          hvis anvendelse i decimalklassedelingen er beskrevet i noten til 82-88.
+          Till&aelig;gstallene vil ogs&aring; kunne finde anvendelse ved anden bibliografisk systematik over sk&oslash;nlitteratur.
+        </div>
+        <div className="section">
+          <i>Till&aelig;gstallene for sk&oslash;nlitteratur</i> best&aring;r af mindst to cifre,
+          hvoraf det f&oslash;rste angiver publiceringssproget,
+          og det sidste dels angiver digtarten,
+          dels markerer antologiform.
+          For b&oslash;rne- og ungdomslitteraturens vedkomne indskydes mellem 1. og 2. ciffer et 9-tal, der markerer m&aring;lgruppen.
+        </div>
+        <div className="section">
+          1. <i>ciffer</i> tildeles efter nedenst&aring;ende tabel:
+          <div><div className="bullet">0</div>Skrifter p&aring; originalsproget</div>
+          <div><div className="bullet">1</div>Skrifter oversat til fremmesprog</div>
+          <div><div className="bullet">2</div>Skrifter oversat til dansk</div>
+        </div>
+        <div className="section">
+          2. <i>ciffer</i> tildeles efter nedenst&aring;ende tabel:
+          <div><div className="bullet">0</div>Digte. Antologier</div>
+          <div><div className="bullet">1</div>Digte. V&aelig;rker af enkelte forfattere
+            <div className="note">
+              Till&aelig;gstalene 0 og 1 omfatter ikke epsisk og dramatisk litteratur p&aring; vers.
+              Kortere fort&aelig;llende digte placeres dog i formgrupperne 0 og 1.
+            </div>
+          </div>
+          <div><div className="bullet">2</div>Skuespil. Antologier</div>
+          <div><div className="bullet">3</div>Skuespil. V&aelig;rker af enkelte forfattere</div>
+          <div><div className="bullet">4</div>Romaner og noveller. Antologier
+            <div className="note">
+              Her anbringes ogs&aring; antologier, sammensat af forskellige digtarter<br />
+              I grupperne 86 og 88 placerer Dansk bogfortegnelse l&aelig;seb&oslash;ger og prosaantologier til
+              skolebrug i en undergruppe med till&aelig;gstallet -046 eller -246.
+              Andre skoleudgaver af sk&oslash;nlitteratur placeres som tilsvarende almindelige udgaver.
+            </div>
+         </div>
+         <div><div className="bullet">5</div>Romaner og noveller f&oslash;r 1901. V&aelig;rker af enkelte forfattere</div>
+         <div><div className="bullet">6</div>Romaner og noveller fra 1901. V&aelig;rker af enkelte forfattere</div>
+         <div><div className="bullet">7</div>Humor, tegneserier etc. Antologier
+           <div className="note">
+             Her anbringes samlinger af vittigheder og humoristiske sm&aring;stykker.
            </div>
+         </div>
+         <div><div className="bullet">8</div>Humor, tegneserier etc. af enkelte forfattere</div>
+         <div><div className="bullet">9</div>B&oslash;rne- og ungdomsb&oslash;ger</div>
+         <div><div className="bullet">90</div>Digte for b&oslash;rn. Antologier</div>
+         <div><div className="bullet">91</div>Digte for b&oslash;rn. Enkelte forfattere</div>
+         <div><div className="bullet">92</div>B&oslash;rnekomedier. Antologier</div>
+         <div><div className="bullet">93</div>B&oslash;rnekomedier. Enkelte forfattere</div>
+         <div><div className="bullet">94</div>Fort&aelig;llinger for b&oslash;rn. Antologier
+           <div className="note">
+             Her anbringes ogs&aring; antologier sammensat af forskellige digtarter.
+           </div>
+         </div>
+         <div><div className="bullet">96</div>Fort&aelig;llinger for b&oslash;rn. Enkelte forfattere</div>
+         <div><div className="bullet">97</div>Humor, tegneserier etc. for b&oslash;rn. Antologier</div>
+         <div><div className="bullet">98</div>Humor, tegneserier etc. for b&oslash;rn. Enkelte forfattere</div>
+        </div>
+        <div className="section">
+          Systemet med till&aelig;gstal, anbragt efter en streg,
+          anvendes ogs&aring; i visse faglitter&aelig;re grupper for at markere en for Dansk bogfortegnelse speciel supplerende underdeling.
+        </div>
+        <div className="section">
+          <i>Faglitteratur &mdash; Till&aelig;gstal med fast betydning</i>
+          <div><div className="bullet">-6</div>Skole- og l&aelig;reb&oslash;ger (inkl. opgavesamlinger)
+            <div className="note">
+              Bruges i 51.3, 86-04 og 88-24.
+            </div>
+          </div>
+          <div><div className="bullet">-61</div>Almindelige l&aelig;reb&oslash;ger, l&aelig;seb&oslash;ger
+            <div className="note">
+              Bruges i 89.2, 89.3 og 89.4.
+            </div>
+          </div>
+          <div><div className="bullet">-62</div>&Oslash;velser og opgaver
+            <div className="note">
+              Bruges i 89.2, 89.3 og 89.4.
+            </div>
+          </div>
+          <div><div className="bullet">-65</div>L&aelig;reb&oslash;ger (inkl. opgavesamlinger) for folkeskolen</div>
+          <div><div className="bullet">-66</div>Andre skole- og l&aelig;reb&oslash;ger (inkl. opgavesamlinger)
+            <div className="note">
+              -65 og -66 bruger i 40, 51, 51.1, 51.5, 51.8, 53, 54, 56 og 90,
+            </div>
+          </div>
+          <div><div className="bullet">-8</div>Udgaver for b&oslash;rn
+            <div className="note">
+              Bruges i 39.12 og 88.99
+            </div>
+          </div>
+          <div><div className="bullet">-99</div>Enkeltbiografier
+            <div className="note">
+              Bruges i alle grupper undtagen de alfabetisk (herunder de geografisk-topografisk) underdelte.
+              Underdeles alfabetisk efter den biograferede persones navn.
+            </div>
+          </div>
+        </div>
+        <div className="section">
+          <i>Faglitteratur &mdash; Till&aelig;gstal med varierende betydning</i>
+          <div><div className="bullet">46.4-0</div>Sj&aelig;lland og M&oslash;n</div>
+          <div><div className="bullet">46.4-1</div>Sams&oslash;</div>
+          <div><div className="bullet">46.4-2</div>Lolland-Falster</div>
+          <div><div className="bullet">46.4-3</div>Bronholm</div>
+          <div><div className="bullet">46.4-4</div>Fyn og de fynske &oslash;er</div>
+          <div><div className="bullet">46.4-5</div>Jylland</div>
+        </div>
+        <div className="section">
+          <div><div className="bullet">72-5</div>Humoristiske og satiriske tegninger af enkelte kunstnere.</div>
+        </div>
+      </div>);
     }
     return null;
   }
--- src/client/components/HierarchyContainer/HierarchyContainer.component.js
+++ src/client/components/HierarchyContainer/HierarchyContainer.component.js
@@ -144,7 +144,7 @@ function HierarchyLevel({hierarchy, Header = 'h2', level = 1, selected, pro, car
   const cartButton = level >= 2 && pro ? <CartButton {...{index, cart}} /> : null;
   const infoChildren = pro & hasChildren ? 'hasChildren' : '';
   const infoDecommissioned = pro & decommissioned ? 'decommissioned' : '';
-  const link_82_88 = pro && isSelected && index === '82-88' ? <Link to={`/help`}><span className="dk5"> Se till&aelig;gstal</span></Link> : null;
+  const link_82_88 = pro && isSelected && index === '82-88' ? <Link to={`/help`}><span className='dk5'> Se till&aelig;gstal</span></Link> : null;
 
   return (
     <div className={`hierarchy-level level level-${level}`}>
