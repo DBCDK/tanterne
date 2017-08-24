e7cd4d5e695448125bb7a16a83505c3ec55c331f
Finn Stausgaard <fvs@dbc.dk>
2017-08-24 14:00:06 +0200

    try setting maxInstances to 1

--- integration/config/sauce.conf.js
+++ integration/config/sauce.conf.js
@@ -1,8 +1,8 @@
 module.exports = {
-  maxInstances: 10,
+  maxInstances: 1,
   capabilities: [
     {
-      maxInstances: 4,
+      maxInstances: 1,
       browserName: 'chrome',
       platform: 'OS X 10.11',
       version: 'latest',
